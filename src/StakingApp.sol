ba194ae1cf809eddfeb415ca6c8a04d78a4bac49
david123456858 <juandavidperaltafuentes@gmail.com>
2026-03-08 20:07:03 -0500

    firts commit

---
+++ src/StakingApp.sol
@@ -0,0 +1,7 @@
+//SPDX-License-1dentifier: LGPL-3.0-onlY
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.28;
+import "../lib/openzeppelin-contracts/contracts/token/ERC20.sol";
+
+contract stakingToken {}
